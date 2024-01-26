46f01084fc0ac0f9523e05f9b1315394414dc420
Corinne Jones <jonescorinne6@gmail.com>
2024-01-25 17:08:29 -0700

    Auth working

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const user = require("../models/User");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const validateRegisterInput = require("../validation/registerValidation"); 
+const jwt = require('jsonwebtoken'); 
+const requireAuth = require("../middleware/permission"); 
 
 //@route    GET /api/auth/test
 //@desc     Test the auth route
@@ -44,8 +46,12 @@ router.post("/register", async(req, res)=>{
         //save user to database
         const savedUser = await newUser.save(); 
 
+        //create obj to spread the data and delete password
+        const userToReturn = {...savedUser._doc};
+        delete userToReturn.password; 
+
         //return new user
-        return res.json(savedUser);
+        return res.json(userToReturn);
     }
     catch(err){
         //error 
@@ -54,5 +60,65 @@ router.post("/register", async(req, res)=>{
         res.status(500).send(err.message); 
     }
 });
+
+//@route    POST /api/auth/login
+//@desc     Login user and return access token
+//@access   Public
+router.post("/login", async(req, res)=>{
+    try {
+        //check for user
+        const user = await User.findOne({
+            email: new RegExp("^" + req.body.email + "$", "i"),
+        }); 
+
+        if(!user){
+            return res.status(400).json({error: "There was a problem with loging credentials"}); 
+        }
+
+        const passwordMatch = await bcrypt.compare(req.body.password, user.password); 
+
+        if(!passwordMatch){
+            return res.status(400).json({error: "There was a problem with loging credentials"}); 
+        }
+
+        const payload = {userId: user._id}; 
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        });
+
+        //set a cookie
+        res.cookie("access-token", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            httpOnly: true, 
+            secure: process.env.NODE_ENV==="production"
+        }); 
+
+        const userToReturn = {...user._doc}; //spread the user
+        delete userToReturn.password; 
+
+        return res.json({
+            token: token, 
+            user: userToReturn,
+        }); 
+
+        // return res.json({passwordMatch: passwordMatch}); 
+    } catch (error) {
+        console.log(error); 
+
+        return res.status(500).send(error.message); 
+    }
+}); 
+
+//@route    GET /api/current
+//@desc     Return the currently authed user
+//@access   Private
+
+router.get("/current", requireAuth, (req, res) =>{
+    if(!req.user){
+        return res.status(401).send("Unauthorized"); 
+    }
+    return res.json(req.user); 
+}); 
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config(); // load environment variables from .env file 
 const express = require("express");
 const mongoose = require("mongoose"); 
+const cookieParser = require("cookie-parser"); 
 
 //import routes
 const authRoute = require("./routes/auth");
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded()); 
+app.use(cookieParser()); 
 
 // define a route that handles HTTP GET requests to the root URL "/"
 app.get("/api", (req, res)=>{

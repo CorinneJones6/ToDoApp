f5071964a1e04ff37869ba4602524383cfde7a1e
Corinne Jones <jonescorinne6@gmail.com>
2024-01-28 19:06:42 -0700

    Global context additions

---
+++ client/src/context/GlobalContext.js
@@ -0,0 +1,105 @@
+import React, {createContext, useContext, useReducer, useEffect} from 'react';
+import axios from 'axios';
+
+//initial state 
+const initialState = {
+    user: null, 
+    fetchingUser: true, 
+    completeToDos: [], 
+    incompleteToDos: [],
+}
+
+//reducer
+const globalReducer = (state, action) =>{
+    switch (action.type){
+        case "SET_USER":
+            return {
+                ...state,
+                user: action.payload,
+                fetchingUser: false,
+            };
+        case "SET_COMPLETE_TODOS":
+            return {
+                 ...state,
+                  completeToDos: action.payload,
+             };
+        case "SET_INCOMPLETE_TODOS":
+            return {
+                 ...state,
+                 incompleteToDos: action.payload,
+             };
+        case "RESET_USER":
+            return {
+                ...state, 
+                user: null,
+                completeToDos: [],
+                incompleteToDos: [],
+                fetchingUser: false, 
+            }
+        default: 
+            return state; 
+    }
+}
+
+//create the context
+export const GlobalContext = createContext(initialState); 
+
+//create provider component
+export const GlobalProvider = (props)=>{
+    const [state, dispatch] = useReducer(globalReducer, initialState); 
+
+    useEffect(() => {
+        getCurrentUser(); 
+    }, []);
+
+    //action: get current user
+    const getCurrentUser = async () => {
+        try{
+            const res = await axios.get("/api/auth/current");
+
+            if(res.data){
+                const toDosRes = await axios.get("/api/todos/current"); 
+
+                if(toDosRes.data){
+                    dispatch({type: "SET_USER", payload: res.data}); 
+                    dispatch({type: "SET_COMPLETE_TODOS", payload: toDosRes.data.complete}); 
+                    dispatch({type: "SET_INCOMPLETE_TODOS", payload: toDosRes.data.incomplete});
+                }
+            } 
+            else {
+                dispatch({type: "RESET_USER"});
+            }
+        }
+        catch(error){
+            console.log(error); 
+            dispatch({type: "RESET_USER"});
+        }
+    }
+
+    const value = {
+       ...state, 
+       getCurrentUser,
+       logout, 
+    };
+
+    return(
+        <GlobalContext.Provider value={value}>
+            {props.children}
+        </GlobalContext.Provider>
+    );
+};
+
+const logout = async () =>{
+    try {
+        await axios.put("/api/auth/logout");
+
+        dispatchEvent({type: "RESET_USER"}); 
+    } catch (error) {
+        console.log(error);
+        dispatchEvent({type: "RESET_USER"});
+    }
+};
+
+export function useGlobalContext() {
+    return useContext(GlobalContext); 
+}
